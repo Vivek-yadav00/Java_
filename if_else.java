7865c938e21fb1e4fb71f37810d0208f49be9de4
Vivek Yadav <vivekyadavboss2004@gmail.com>
2025-02-04 23:17:31 +0530

    new things

---
+++ if_else.java
@@ -0,0 +1,10 @@
+public class if_else {
+    public static void main(String[] args) {
+        int a = 10;
+        if (a > 10) {
+            System.out.println("Output is: " + a + 5);
+        } else {
+            System.out.println("output is: " + (a - 5));
+        }
+    }
+}
