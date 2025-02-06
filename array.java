4acad5b47dc4e3e84a7f29164b71a2b222d09c8e
Vivek Yadav <vivekyadavboss2004@gmail.com>
2025-02-06 23:04:54 +0530

    array

---
+++ array.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] marks=new int[5];
+        for (int i=0;i<5;i++){
+            System.out.println("Enter the marks of student "+(i+1));
+            marks[i]=sc.nextInt();
+        }
+        for (int i=0;i<5;i++){
+            System.out.println("The marks of student "+(i+1)+" is "+marks[i]);
+        }
+        sc.close();
+    }
+}
