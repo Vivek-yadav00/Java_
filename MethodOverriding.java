be5bb932d397a6429699f139176fc82f0263ba98
Vivek Yadav <vivekyadavboss2004@gmail.com>
2025-03-05 22:46:15 +0530

    MethodOverriding

---
+++ MethodOverriding.java
@@ -0,0 +1,28 @@
+class Parent {
+    public int a;
+
+    public void show() {
+        System.out.println("Parent class show method");
+    }
+}
+
+class Child extends Parent {
+    @Override
+    public void show() {
+        System.out.println("Child class show method");
+    }
+
+    public void display() {
+        System.out.println("Child class display method");
+    }
+}
+
+public class MethodOverriding {
+    public static void main(String[] args) {
+        Parent p = new Parent();
+        p.show();
+        Child c = new Child();
+        c.show();
+        c.display();
+    }
+}
