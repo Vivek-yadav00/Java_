8d1b1e31daa9c87cad8f2a0ba9bb6cb57ad04ac5
Vivek Yadav <vivekyadavboss2004@gmail.com>
2025-05-04 00:18:35 +0530

    HashSet

---
+++ Hashing.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class Hashing {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(85);
+        set.add(12);
+        set.add(14);
+        set.add(1);
+        set.add(14);
+        System.out.println(set);
+    }
+}
